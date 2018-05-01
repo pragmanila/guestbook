a8306a4b38b8c4a3d1cba99bf707ba76eeb3b8e7
xshame <kevin.camu07@gmail.com>
2018-05-01 17:11:03 +0800

    added result for the guestbook script

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,4 +24,71 @@ $(document).ready(function(){
       }
     });
 
+    //SEND
+    const button = document.getElementById('btn-send');
+    const name = document.getElementById('name');
+    const subject = document.getElementById('subject');
+    const message1 = document.getElementById('message');
+    const list = document.querySelector('.guest-list .list-content');
+    const addForms = document.forms['add-guest'];
+
+    button.addEventListener('click',function(e){
+        e.preventDefault();
+        if (name.value == "" || subject.value == "" || message1.value == "") {
+          if (name.value == "") {
+            alert("Please input a Name");
+            name.focus();
+          } else if (subject.value == "") {
+            alert("Please input a Subject ");
+            subject.focus();
+          } else if (message1.value == "") {
+            alert("Please input a Message ");
+            message1.focus();
+          }
+            return false;
+
+        } else {
+          // get the elements
+          const guestUser = document.createElement('div');
+          const userContent = document.createElement('div');
+          const titleUser = document.createElement('h3');
+          const user = document.createElement('h3');
+          const date = document.createElement('p');
+          const message = document.createElement('p');
+
+          //call and Add Class
+          guestUser.classList.add('guest-user');
+          userContent.classList.add('user-content');
+          titleUser.classList.add('title-user');
+          user.classList.add('users');
+          date.classList.add('date');
+          message.classList.add('message');
+
+          //Append Dom
+          guestUser.appendChild(userContent);
+          userContent.appendChild(titleUser);
+          userContent.appendChild(user);
+          userContent.appendChild(date);
+          userContent.appendChild(message);
+          list.appendChild(guestUser);
+
+          //add Content
+          user.textContent = name.value;
+          titleUser.textContent = subject.value;
+          var dateToday = new Date();
+          var month = dateToday.getMonth();
+          var year = dateToday.getFullYear();
+          var day = dateToday.getDate();
+          year = year.toString().substr(-2);
+          month = (month + 1).toString();
+          date.textContent = month + " / " + day + " / " + year + " - " + dateToday.getHours() + ":" + dateToday.getMinutes() + ":" + dateToday.getSeconds();
+
+          message.textContent = message1.value;
+
+          //to clear form
+          addForms.reset();
+          }
+
+    });
+
 });
